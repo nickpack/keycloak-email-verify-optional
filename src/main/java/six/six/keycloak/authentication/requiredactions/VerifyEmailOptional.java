9e19a6cedaa46e94f5bdcdd241e8435a6e677266
Nick Pack <nick@nickpack.com>
2017-08-11 11:33:10 +0100

    Initial commit

---
+++ src/main/java/six/six/keycloak/authentication/requiredactions/VerifyEmailOptional.java
@@ -0,0 +1,119 @@
+package six.six.keycloak.authentication.requiredactions;
+
+/**
+ * Created by nickpack on 11/08/2017.
+ */
+
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.core.Response;
+
+import org.keycloak.authentication.RequiredActionContext;
+import org.keycloak.authentication.actiontoken.verifyemail.VerifyEmailActionToken;
+import org.keycloak.authentication.requiredactions.VerifyEmail;
+import org.keycloak.common.util.Time;
+import org.keycloak.email.EmailException;
+import org.keycloak.email.EmailTemplateProvider;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.events.EventType;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.*;
+import org.keycloak.services.Urls;
+import org.keycloak.services.validation.Validation;
+
+import org.keycloak.sessions.AuthenticationSessionModel;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.core.*;
+
+
+public class VerifyEmailOptional extends VerifyEmail {
+    private static final Logger logger = Logger.getLogger(VerifyEmailOptional.class);
+
+    @Override
+    public void evaluateTriggers(RequiredActionContext context) {
+        String doThisLater = (String) context.getSession().getAttribute("skip_email_verify");
+
+        if (!context.getUser().isEmailVerified() && doThisLater == null) {
+            context.getUser().addRequiredAction(UserModel.RequiredAction.VERIFY_EMAIL);
+            logger.debug("User is required to verify email");
+        }
+    }
+
+    @Override
+    public void requiredActionChallenge(RequiredActionContext context) {
+        String doThisLater = (String) context.getSession().getAttribute("skip_email_verify");
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
+
+        if (context.getUser().isEmailVerified() || doThisLater != null) {
+            context.success();
+
+            if (context.getUser().isEmailVerified()) {
+                authSession.removeAuthNote(Constants.VERIFY_EMAIL_KEY);
+            }
+            return;
+        }
+
+        String email = context.getUser().getEmail();
+        if (Validation.isBlank(email)) {
+            context.ignore();
+            return;
+        }
+
+        LoginFormsProvider loginFormsProvider = context.form();
+        Response challenge;
+
+        // Do not allow resending e-mail by simple page refresh, i.e. when e-mail sent, it should be resent properly via email-verification endpoint
+        if (!Objects.equals(authSession.getAuthNote(Constants.VERIFY_EMAIL_KEY), email)) {
+            authSession.setAuthNote(Constants.VERIFY_EMAIL_KEY, email);
+            EventBuilder event = context.getEvent().clone().event(EventType.SEND_VERIFY_EMAIL).detail(Details.EMAIL, email);
+            challenge = sendVerifyEmail(context.getSession(), loginFormsProvider, context.getUser(), context.getAuthenticationSession(), event);
+        } else {
+            context.getSession().setAttribute("skip_email_verify", "true");
+            challenge = loginFormsProvider.createResponse(UserModel.RequiredAction.VERIFY_EMAIL);
+        }
+
+        context.challenge(challenge);
+    }
+
+
+    @Override
+    public void processAction(RequiredActionContext context) {
+        logger.debugf("Re-sending email requested for user: %s", context.getUser().getUsername());
+
+        // This will allow user to re-send email again
+        context.getAuthenticationSession().removeAuthNote(Constants.VERIFY_EMAIL_KEY);
+
+        requiredActionChallenge(context);
+    }
+
+    private Response sendVerifyEmail(KeycloakSession session, LoginFormsProvider forms, UserModel user, AuthenticationSessionModel authSession, EventBuilder event) throws UriBuilderException, IllegalArgumentException {
+        RealmModel realm = session.getContext().getRealm();
+        UriInfo uriInfo = session.getContext().getUri();
+
+        int validityInSecs = realm.getActionTokenGeneratedByUserLifespan();
+        int absoluteExpirationInSecs = Time.currentTime() + validityInSecs;
+
+        VerifyEmailActionToken token = new VerifyEmailActionToken(user.getId(), absoluteExpirationInSecs, authSession.getId(), user.getEmail());
+        UriBuilder builder = Urls.actionTokenBuilder(uriInfo.getBaseUri(), token.serialize(session, realm, uriInfo));
+        String link = builder.build(realm.getName()).toString();
+        long expirationInMinutes = TimeUnit.SECONDS.toMinutes(validityInSecs);
+
+        try {
+            session
+                    .getProvider(EmailTemplateProvider.class)
+                    .setRealm(realm)
+                    .setUser(user)
+                    .sendVerifyEmail(link, expirationInMinutes);
+            event.success();
+        } catch (EmailException e) {
+            logger.error("Failed to send verification email", e);
+            event.error(Errors.EMAIL_SEND_FAILED);
+        }
+
+        return forms.createResponse(UserModel.RequiredAction.VERIFY_EMAIL);
+    }
+}
